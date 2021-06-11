722d56f397f378e6fa140450741b7f2dca46a4ef
wyznu <wiz51ceae@gmail.com>
2021-06-11 20:01:25 +0700

    update data

--- lib/view/view_info.dart
+++ lib/view/view_info.dart
@@ -1,57 +1,76 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ViewInfo{
-
-  
+class ViewInfo {
   ListView tabInfo() {
-    return ListView(children: [
-            Column(children: [
-              Text(''),
-              ElevatedButton(onPressed: (){}, child: Text('a'))
-            ],),
-          ],);
+    return ListView(
+      children: [
+        Column(
+          children: [
+            Text(''),
+            ElevatedButton(onPressed: () {}, child: Text('a'))
+          ],
+        ),
+      ],
+    );
   }
 
-ListView dataFirestorenama(AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+  ListView dataTeam(
+      AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshotTeam) {
     return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data!.docs[index]
-                          .data()['nama']
-                          .toString()),
-                    );
-                  },
-                );
+      itemCount: snapshotTeam.data!.docs.length,
+      itemBuilder: (context, index) {
+        return Container(height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(snapshotTeam.data!.docs[index].data()['tim'].toString()),
+              Text(snapshotTeam.data!.docs[index].data()['alamat'].toString()),
+              Text(snapshotTeam.data!.docs[index].data()['kota'].toString()),
+              Text(snapshotTeam.data!.docs[index].data()['tahun'].toString()),
+            ],
+          ),
+        );
+      },
+    );
   }
 
-  ListView dataFirestoreumur(AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+  ListView dataInfo(
+      AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshotInfo) {
     return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data!.docs[index]
-                          .data()['umur']
-                          .toString()),
-                    );
-                  },
-                );
+      itemCount: snapshotInfo.data!.docs.length,
+      itemBuilder: (context, index) {
+        return Container(height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(snapshotInfo.data!.docs[index].data()['skor'].toString()),
+              Text(snapshotInfo.data!.docs[index].data()['waktu gol'].toString()),
+            ],
+          ),
+        );
+      },
+    );
   }
 
-
-ListView dataFirestore1(AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot1) {
+  ListView dataPemain(
+      AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshotPemain) {
     return ListView.builder(
-                  itemCount: snapshot1.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot1.data!.docs[index]
-                          .data()['hari']
-                          .toString()),
-                    );
-                  },
-                );
+      itemCount: snapshotPemain.data!.docs.length,
+      itemBuilder: (context, index) {
+        return Container(height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text('Nama Pemain : ' + snapshotPemain.data!.docs[index].data()['nama'].toString()),
+              Text('berat Pemain : ' + snapshotPemain.data!.docs[index].data()['berat'].toString()),
+              Text('No Punggung : ' + snapshotPemain.data!.docs[index].data()['no'].toString()),
+              Text('Posisi : ' + snapshotPemain.data!.docs[index].data()['posisi'].toString()),
+              Text('Tinggi Badan : ' + snapshotPemain.data!.docs[index].data()['tinggi'].toString()),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
-
-
--- lib/view/xyz_view.dart
+++ lib/view/xyz_view.dart
@@ -2,12 +2,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:xyz/view/view_info.dart';
 
-class XyzView extends StatelessWidget {
+class XyzView extends StatefulWidget {
+  @override
+  _XyzViewState createState() => _XyzViewState();
+}
+
+class _XyzViewState extends State<XyzView> {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  Stream<QuerySnapshot<Map<String, dynamic>>> collectionStream =
-      FirebaseFirestore.instance.collection('users').snapshots();
-  Stream<QuerySnapshot<Map<String, dynamic>>> collectionStream1 =
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> collectionStreamTeam =
+      FirebaseFirestore.instance.collection('team').snapshots();
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> collectionStreamInfo =
       FirebaseFirestore.instance.collection('info').snapshots();
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> collectionStreamPemain =
+      FirebaseFirestore.instance.collection('pemain').snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -21,22 +31,28 @@ class XyzView extends StatelessWidget {
             bottom: TabBar(tabs: [
               Text('info'),
               Text('Team'),
-              Text('skor'),
+              Text('pemain'),
             ]),
           ),
           body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-            stream: collectionStream,
-            builder: (context, snapshot) {
+            stream: collectionStreamTeam,
+            builder: (context, snapshotTeam) {
               return TabBarView(
                 children: [
-                  ViewInfo().dataFirestorenama(snapshot),
-                  ViewInfo().dataFirestoreumur(snapshot),
                   StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                    stream: collectionStream1,
-                    builder: (context, snapshot1) {
-                      return ViewInfo().dataFirestore1(snapshot1);
+                    stream: collectionStreamInfo,
+                    builder: (context, snapshotInfo) {
+                      return ViewInfo().dataInfo(snapshotInfo);
                     },
                   ),
+                  ViewInfo().dataTeam(snapshotTeam),
+                  StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                    stream: collectionStreamPemain,
+                    builder: (context, snapshotPemain) {
+                      return ViewInfo().dataPemain(snapshotPemain);
+                    },
+                  ),
+                  
                 ],
               );
             },
