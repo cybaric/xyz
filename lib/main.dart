aa5bf074381cef164926aef0212f4bccf981c286
wyznu <wiz51ceae@gmail.com>
2021-06-11 16:00:43 +0700

    menghubungkan ke firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:xyz/view/xyz_view.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  //const MyApp({ Key? key }) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      home: XyzView(),
-      
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder(
+          // Initialize FlutterFire:
+          future: _initialization,
+          builder: (context, snapshot) {
+            // Check for errors
+            if (snapshot.hasError) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text('Error'),
+                ),
+              );
+            }
+
+            // Once complete, show your application
+            if (snapshot.connectionState == ConnectionState.done) {
+              return XyzView();
+            }
+
+            // Otherwise, show something whilst waiting for initialization to complete
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Loading'),
+              ),
+            );
+          },
+        ));
   }
 }
